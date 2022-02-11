373b5667f3431276a05b17f52511e4c360f70cde
Adam Lindgren <adam@adamlindgren.com>
2022-02-11 14:53:11 -0500

    finished everything except question 5

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const form = document.querySelector("#parking-form")
 const msgDiv = document.querySelector("#msg")
+const ccNumber = document.querySelector("#credit-card")
 
 //creating date for html validation mins and maxes
 const today = new Date()
@@ -26,7 +27,6 @@ startDate.min = todayDate
 startDate.value = todayDate
 
 
-
 let formIsValid
 
 //submitting the form
@@ -36,19 +36,61 @@ form.addEventListener("submit", function(event) {
 })
 
 function validateForm() {
-    // removeMessage()
-    formIsValid = true
-
-    // ValidateCC()
-    // ValidateExp()
-
-    if (formIsValid) {
-        let days = document.querySelector("#days")
-        document.getElementById("total").innerText = `Sucess! Total Amount Paid: $${5 * days.value}`
+    removeMessage()
+    validateExp()
+    if (validateCC(ccNumber.value)) {
+        if (formIsValid) {
+            let days = document.querySelector("#days")
+            document.getElementById("total").innerText = `Sucess! Total Amount Paid: $${5 * days.value}`
+        }
+    } else {
+        showMessage('Please enter a valid Credit Card number')
+        formIsValid = false
     }
+}
+
+
+function validateCC(number) {
+    var regex = new RegExp("^[0-9]{16}$");
+    if (!regex.test(number))
+        return false;
+
+    return luhnCheck(number);
+}
+
+function luhnCheck(val) {
+    var sum = 0;
+    for (var i = 0; i < val.length; i++) {
+        var intVal = parseInt(val.substr(i, 1));
+        if (i % 2 == 0) {
+            intVal *= 2;
+            if (intVal > 9) {
+                intVal = 1 + (intVal % 10);
+            }
+        }
+        sum += intVal;
+    }
+    return (sum % 10) == 0;
 }
 
 const expDate = form.querySelector("#expiration")
 function validateExp() {
-    
+    expYear = expDate.value.slice(3)
+    if (expYear >= yyyy.toString().slice(2)) {
+        console.log("expiration date is valid")
+    } else {
+        showMessage('Expiration Date must be in the future')
+        formIsValid = false
+    }
+}
+
+function removeMessage() {
+    if (msgDiv) {
+        msgDiv.innerHTML = ''
+        formIsValid = true
+    }
+}
+
+function showMessage(message) {
+    msgDiv.innerHTML = `<div class="YIKES">${message}</div>`
 }
